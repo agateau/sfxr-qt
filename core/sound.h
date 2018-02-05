ea02fd5dc28a7c6f22d5bcf4967f544c188a115a
Aurelien Gateau <mail@agateau.com>
2018-02-05 19:08:09 +0100

    Make it possible for a sound to init itself from another one

--- core/sound.h
+++ core/sound.h
@@ -9,8 +9,11 @@ public:
     explicit Sound(QObject* parent = nullptr);
 
     void resetParams();
+    Q_INVOKABLE void fromOther(Sound* other);
     Q_INVOKABLE bool load(const QUrl& url);
     Q_INVOKABLE bool save(const QUrl& url);
 };
+
+Q_DECLARE_METATYPE(Sound*)
 
 #endif // SOUND_H
