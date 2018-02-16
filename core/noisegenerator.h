4cd05f7512ef7acebc5fe35608a1cde4583f7520
Aurelien Gateau <mail@agateau.com>
2018-02-16 22:50:47 +0100

    Document NoiseGenerator

--- core/noisegenerator.h
+++ core/noisegenerator.h
@@ -1,9 +1,21 @@
 #ifndef NOISEGENERATOR_H
 #define NOISEGENERATOR_H
 
+/**
+ * Generates a reproducible sequence of random values between -1 and 1
+ * for a period.
+ *
+ * The generator returns `sampleCount` (the constructor argument) different
+ * values, stretched on the period.
+ *
+ * `alpha` the argument of the get() method, is a number between 0 and 1,
+ * where 0 represents the start of the period and 1 the end.
+ *
+ * To reproduce the same sequence, call reset()
+ */
 class NoiseGenerator {
 public:
-    explicit NoiseGenerator(int length);
+    explicit NoiseGenerator(int sampleCount);
     void reset();
     float get(float alpha);
 private:
