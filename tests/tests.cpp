fd1c510c93e1ff90f65d5d4af675816917fe878c
Aurelien Gateau <mail@agateau.com>
2019-09-03 23:05:11 +0200

    Make core independent of QML
    Move Generator to ui.

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,12 +1,12 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch2/catch.hpp>
 
-#include <QApplication>
+#include <QCoreApplication>
 #include <QtTest>
 
 int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);
+    QCoreApplication app(argc, argv);
     QTEST_SET_MAIN_SOURCE_PATH
     return Catch::Session().run(argc, argv);
 }
