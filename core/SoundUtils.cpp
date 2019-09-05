3ec1436d3328b875c713c4a5d2cc2459bc6b8565
Aurelien Gateau <mail@agateau.com>
2019-09-05 19:16:43 +0200

    Fix qreal vs float warnings

--- core/SoundUtils.cpp
+++ core/SoundUtils.cpp
@@ -2,30 +2,28 @@
 
 #include <QMetaProperty>
 
-#include <math.h>
-
 #include <Sound.h>
 
 namespace SoundUtils {
 
 inline int rnd(int n) {
-    return rand() % (n + 1);
+    return std::rand() % (n + 1);
 }
 
-inline float frnd(float range) {
-    return (float)rnd(10000) / 10000 * range;
+inline qreal frnd(qreal range) {
+    return qreal(rnd(10000)) / 10000.0 * range;
 }
 
 std::unique_ptr<Sound> generatePickup() {
     auto sound = std::make_unique<Sound>();
-    sound->setBaseFrequency(0.4f + frnd(0.5f));
-    sound->setAttackTime(0.0f);
-    sound->setSustainTime(frnd(0.1f));
-    sound->setDecayTime(0.1f + frnd(0.4f));
-    sound->setSustainPunch(0.3f + frnd(0.3f));
+    sound->setBaseFrequency(0.4 + frnd(0.5));
+    sound->setAttackTime(0.0);
+    sound->setSustainTime(frnd(0.1));
+    sound->setDecayTime(0.1 + frnd(0.4));
+    sound->setSustainPunch(0.3 + frnd(0.3));
     if (rnd(1)) {
-        sound->setChangeSpeed(0.5f + frnd(0.2f));
-        sound->setChangeAmount(0.2f + frnd(0.4f));
+        sound->setChangeSpeed(0.5 + frnd(0.2));
+        sound->setChangeAmount(0.2 + frnd(0.4));
     }
     return sound;
 }
@@ -35,36 +33,36 @@ std::unique_ptr<Sound> generateLaser() {
     sound->setWaveForm(WaveForm::random({WaveForm::Square,
                                          WaveForm::Sawtooth,
                                          WaveForm::Sine}));
-    sound->setBaseFrequency(0.5f + frnd(0.5f));
-    sound->setMinFrequency(sound->baseFrequency() - 0.2f - frnd(0.6f));
-    if (sound->minFrequency() < 0.2f) {
-        sound->setMinFrequency(0.2f);
+    sound->setBaseFrequency(0.5 + frnd(0.5));
+    sound->setMinFrequency(sound->baseFrequency() - 0.2 - frnd(0.6));
+    if (sound->minFrequency() < 0.2) {
+        sound->setMinFrequency(0.2);
     }
-    sound->setSlide(-0.15f - frnd(0.2f));
+    sound->setSlide(-0.15 - frnd(0.2));
     if (rnd(2) == 0) {
-        sound->setBaseFrequency(0.3f + frnd(0.6f));
-        sound->setMinFrequency(frnd(0.1f));
-        sound->setSlide(-0.35f - frnd(0.3f));
+        sound->setBaseFrequency(0.3 + frnd(0.6));
+        sound->setMinFrequency(frnd(0.1));
+        sound->setSlide(-0.35 - frnd(0.3));
     }
     if (rnd(1)) {
-        sound->setSquareDuty(frnd(0.5f));
-        sound->setDutySweep(frnd(0.2f));
+        sound->setSquareDuty(frnd(0.5));
+        sound->setDutySweep(frnd(0.2));
     } else {
-        sound->setSquareDuty(0.4f + frnd(0.5f));
-        sound->setDutySweep(-frnd(0.7f));
+        sound->setSquareDuty(0.4 + frnd(0.5));
+        sound->setDutySweep(-frnd(0.7));
     }
-    sound->setAttackTime(0.0f);
-    sound->setSustainTime(0.1f + frnd(0.2f));
-    sound->setDecayTime(frnd(0.4f));
+    sound->setAttackTime(0.0);
+    sound->setSustainTime(0.1 + frnd(0.2));
+    sound->setDecayTime(frnd(0.4));
     if (rnd(1)) {
-        sound->setSustainPunch(frnd(0.3f));
+        sound->setSustainPunch(frnd(0.3));
     }
     if (rnd(2) == 0) {
-        sound->setPhaserOffset(frnd(0.2f));
-        sound->setPhaserSweep(-frnd(0.2f));
+        sound->setPhaserOffset(frnd(0.2));
+        sound->setPhaserSweep(-frnd(0.2));
     }
     if (rnd(1)) {
-        sound->setHpFilterCutoff(frnd(0.3f));
+        sound->setHpFilterCutoff(frnd(0.3));
     }
     return sound;
 }
@@ -73,34 +71,34 @@ std::unique_ptr<Sound> generateExplosion() {
     auto sound = std::make_unique<Sound>();
     sound->setWaveForm(WaveForm::Noise);
     if (rnd(1)) {
-        sound->setBaseFrequency(0.1f + frnd(0.4f));
-        sound->setSlide(-0.1f + frnd(0.4f));
+        sound->setBaseFrequency(0.1 + frnd(0.4));
+        sound->setSlide(-0.1 + frnd(0.4));
     } else {
-        sound->setBaseFrequency(0.2f + frnd(0.7f));
-        sound->setSlide(-0.2f - frnd(0.2f));
+        sound->setBaseFrequency(0.2 + frnd(0.7));
+        sound->setSlide(-0.2 - frnd(0.2));
     }
     sound->setBaseFrequency(sound->baseFrequency() * sound->baseFrequency());
     if (rnd(4) == 0) {
-        sound->setSlide(0.0f);
+        sound->setSlide(0.0);
     }
     if (rnd(2) == 0) {
-        sound->setRepeatSpeed(0.3f + frnd(0.5f));
+        sound->setRepeatSpeed(0.3 + frnd(0.5));
     }
-    sound->setAttackTime(0.0f);
-    sound->setSustainTime(0.1f + frnd(0.3f));
-    sound->setDecayTime(frnd(0.5f));
+    sound->setAttackTime(0.0);
+    sound->setSustainTime(0.1 + frnd(0.3));
+    sound->setDecayTime(frnd(0.5));
     if (rnd(1) == 0) {
-        sound->setPhaserOffset(-0.3f + frnd(0.9f));
-        sound->setPhaserSweep(-frnd(0.3f));
+        sound->setPhaserOffset(-0.3 + frnd(0.9));
+        sound->setPhaserSweep(-frnd(0.3));
     }
-    sound->setSustainPunch(0.2f + frnd(0.6f));
+    sound->setSustainPunch(0.2 + frnd(0.6));
     if (rnd(1)) {
-        sound->setVibratoDepth(frnd(0.7f));
-        sound->setVibratoSpeed(frnd(0.6f));
+        sound->setVibratoDepth(frnd(0.7));
+        sound->setVibratoSpeed(frnd(0.6));
     }
     if (rnd(2) == 0) {
-        sound->setChangeSpeed(0.6f + frnd(0.3f));
-        sound->setChangeAmount(0.8f - frnd(1.6f));
+        sound->setChangeSpeed(0.6 + frnd(0.3));
+        sound->setChangeAmount(0.8 - frnd(1.6));
     }
     return sound;
 }
@@ -110,23 +108,23 @@ std::unique_ptr<Sound> generatePowerup() {
     if (rnd(1)) {
         sound->setWaveForm(WaveForm::Sawtooth);
     } else {
-        sound->setSquareDuty(frnd(0.6f));
+        sound->setSquareDuty(frnd(0.6));
     }
     if (rnd(1)) {
-        sound->setBaseFrequency(0.2f + frnd(0.3f));
-        sound->setSlide(0.1f + frnd(0.4f));
-        sound->setRepeatSpeed(0.4f + frnd(0.4f));
+        sound->setBaseFrequency(0.2 + frnd(0.3));
+        sound->setSlide(0.1 + frnd(0.4));
+        sound->setRepeatSpeed(0.4 + frnd(0.4));
     } else {
-        sound->setBaseFrequency(0.2f + frnd(0.3f));
-        sound->setSlide(0.05f + frnd(0.2f));
+        sound->setBaseFrequency(0.2 + frnd(0.3));
+        sound->setSlide(0.05 + frnd(0.2));
         if (rnd(1)) {
-            sound->setVibratoDepth(frnd(0.7f));
-            sound->setVibratoSpeed(frnd(0.6f));
+            sound->setVibratoDepth(frnd(0.7));
+            sound->setVibratoSpeed(frnd(0.6));
         }
     }
-    sound->setAttackTime(0.0f);
-    sound->setSustainTime(frnd(0.4f));
-    sound->setDecayTime(0.1f + frnd(0.4f));
+    sound->setAttackTime(0.0);
+    sound->setSustainTime(frnd(0.4));
+    sound->setDecayTime(0.1 + frnd(0.4));
     return sound;
 }
 
@@ -136,15 +134,15 @@ std::unique_ptr<Sound> generateHitHurt() {
                                          WaveForm::Sawtooth,
                                          WaveForm::Noise}));
     if (sound->waveForm() == WaveForm::Square) {
-        sound->setSquareDuty(frnd(0.6f));
+        sound->setSquareDuty(frnd(0.6));
     }
-    sound->setBaseFrequency(0.2f + frnd(0.6f));
-    sound->setSlide(-0.3f - frnd(0.4f));
-    sound->setAttackTime(0.0f);
-    sound->setSustainTime(frnd(0.1f));
-    sound->setDecayTime(0.1f + frnd(0.2f));
+    sound->setBaseFrequency(0.2 + frnd(0.6));
+    sound->setSlide(-0.3 - frnd(0.4));
+    sound->setAttackTime(0.0);
+    sound->setSustainTime(frnd(0.1));
+    sound->setDecayTime(0.1 + frnd(0.2));
     if (rnd(1)) {
-        sound->setHpFilterCutoff(frnd(0.3f));
+        sound->setHpFilterCutoff(frnd(0.3));
     }
     return sound;
 }
@@ -152,17 +150,17 @@ std::unique_ptr<Sound> generateHitHurt() {
 std::unique_ptr<Sound> generateJump() {
     auto sound = std::make_unique<Sound>();
     sound->setWaveForm(WaveForm::Square);
-    sound->setSquareDuty(frnd(0.6f));
-    sound->setBaseFrequency(0.3f + frnd(0.3f));
-    sound->setSlide(0.1f + frnd(0.2f));
-    sound->setAttackTime(0.0f);
-    sound->setSustainTime(0.1f + frnd(0.3f));
-    sound->setDecayTime(0.1f + frnd(0.2f));
+    sound->setSquareDuty(frnd(0.6));
+    sound->setBaseFrequency(0.3 + frnd(0.3));
+    sound->setSlide(0.1 + frnd(0.2));
+    sound->setAttackTime(0.0);
+    sound->setSustainTime(0.1 + frnd(0.3));
+    sound->setDecayTime(0.1 + frnd(0.2));
     if (rnd(1)) {
-        sound->setHpFilterCutoff(frnd(0.3f));
+        sound->setHpFilterCutoff(frnd(0.3));
     }
     if (rnd(1)) {
-        sound->setLpFilterCutoff(1.0f - frnd(0.6f));
+        sound->setLpFilterCutoff(1.0 - frnd(0.6));
     }
     return sound;
 }
@@ -171,13 +169,13 @@ std::unique_ptr<Sound> generateBlipSelect() {
     auto sound = std::make_unique<Sound>();
     sound->setWaveForm(WaveForm::random({WaveForm::Square, WaveForm::Sawtooth}));
     if (sound->waveForm() == WaveForm::Square) {
-        sound->setSquareDuty(frnd(0.6f));
+        sound->setSquareDuty(frnd(0.6));
     }
-    sound->setBaseFrequency(0.2f + frnd(0.4f));
-    sound->setAttackTime(0.0f);
-    sound->setSustainTime(0.1f + frnd(0.1f));
-    sound->setDecayTime(frnd(0.2f));
-    sound->setHpFilterCutoff(0.1f);
+    sound->setBaseFrequency(0.2 + frnd(0.4));
+    sound->setAttackTime(0.0);
+    sound->setSustainTime(0.1 + frnd(0.1));
+    sound->setDecayTime(frnd(0.2));
+    sound->setHpFilterCutoff(0.1);
     return sound;
 }
 
@@ -186,7 +184,7 @@ void mutate(Sound* sound) {
     for (int i = 0; i < mo.propertyCount(); ++i) {
         QMetaProperty property = mo.property(i);
         if (property.type() == QVariant::Double) {
-            qreal value = property.read(sound).toReal() + frnd(0.1f) - 0.05f;
+            qreal value = property.read(sound).toReal() + frnd(0.1) - 0.05;
             property.write(sound, value);
         }
     }
