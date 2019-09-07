ce939faf3248c143a1e85b8a5500080306b8cf7a
Aurelien Gateau <mail@agateau.com>
2019-09-07 23:00:09 +0200

    Explicitly set values of the WaveForm enum

--- core/WaveForm.h
+++ core/WaveForm.h
@@ -8,11 +8,11 @@ Q_NAMESPACE
 
 // Must be kept in sync with WaveFormSelector.qml
 enum Enum {
-    Square,
-    Sawtooth,
-    Sine,
-    Noise,
-    Last = Noise,
+    // These 4 values must not change to keep compatibility with .sfxr files
+    Square=0,
+    Sawtooth=1,
+    Sine=2,
+    Noise=3,
 };
 
 Enum random(const std::vector<Enum>& values);
