d26d1f708ab3fb6356ed4f5ced0f4dcac2db3701
Aurelien Gateau <mail@agateau.com>
2019-09-20 08:12:18 +0200

    Split main()

--- ui/main.cpp
+++ ui/main.cpp
@@ -9,19 +9,15 @@
 #include "WavSaver.h"
 #include "Result.h"
 
-int main(int argc, char* argv[]) {
-    QApplication app(argc, argv);
-    app.setOrganizationDomain("agateau.com");
-    app.setApplicationName("sfxr-qt");
-    app.setApplicationDisplayName("SFXR Qt");
-
+static QIcon createIcon() {
     QIcon icon;
     for (int size : {16, 32, 48}) {
         icon.addFile(QString(":/icons/sfxr-qt-%1.png").arg(size));
     }
-    app.setWindowIcon(icon);
+    return icon;
+}
 
-    QQmlApplicationEngine engine;
+static void registerQmlTypes() {
     qmlRegisterType<Sound>("sfxr", 1, 0, "Sound");
     qmlRegisterType<SoundPlayer>("sfxr", 1, 0, "SoundPlayer");
     qmlRegisterType<Generator>("sfxr", 1, 0, "Generator");
@@ -29,7 +25,17 @@ int main(int argc, char* argv[]) {
     qmlRegisterType<WavSaver>("sfxr", 1, 0, "WavSaver");
     WaveForm::registerType();
     Result::registerType();
+}
 
+int main(int argc, char* argv[]) {
+    QApplication app(argc, argv);
+    app.setOrganizationDomain("agateau.com");
+    app.setApplicationName("sfxr-qt");
+    app.setApplicationDisplayName("SFXR Qt");
+    app.setWindowIcon(createIcon());
+
+    registerQmlTypes();
+    QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
 
     return app.exec();
